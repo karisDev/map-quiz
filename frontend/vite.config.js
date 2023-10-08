9840fd270b321f34f69292b1302ebdb29c6ea436
karisDev <karjaakirill@gmail.com>
2023-10-08 15:54:26 +0300

    final fix for docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/map-quiz`,
+  base: `/map-quiz/`,
   // host localhost
   server: {
     host: true,
